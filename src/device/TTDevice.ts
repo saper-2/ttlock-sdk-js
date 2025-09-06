7a31feb265b65d5f3b4189ed10a4ef2358664d8f
saper-2 <sapers.2@gmail.com>
2025-09-06 23:37:57 +0200

    Fixed TTDevice + toJSON

--- src/device/TTDevice.ts
+++ src/device/TTDevice.ts
@@ -14,27 +14,18 @@ export class TTDevice extends EventEmitter {
   firmware: string = "unknown";
   address: string = "";
   rssi: number = 0;
-  /** @type {byte} */
   protocolType: number = 0;
-  /** @type {byte} */
   protocolVersion: number = 0;
-  /** @type {byte} */
   scene: number = 0;
-  /** @type {byte} */
   groupId: number = 0;
-  /** @type {byte} */
   orgId: number = 0;
-  /** @type {byte} */
   lockType: LockType = LockType.UNKNOWN;
   isTouch: boolean = false;
   isUnlock: boolean = false;
   hasEvents: boolean = true;
   isSettingMode: boolean = false;
-  /** @type {byte} */
   txPowerLevel: number = 0;
-  /** @type {byte} */
   batteryCapacity: number = -1;
-  /** @type {number} */
   date: number = 0;
   isWristband: boolean = false;
   isRoomLock: boolean = false;
@@ -51,34 +42,39 @@ export class TTDevice extends EventEmitter {
   disconnectStatus: number = 0;
   parkStatus: number = 0;
 
-  toJSON(asObject:boolean = false): string | Object {
-    const temp = new TTDevice();
-    var json = {};
-    
-    // exclude keys that we don't need from the export
+  toJSON(asObject: boolean = false): string | Object {
+    const json: { [key: string]: any } = {};
+
     const excludedKeys = new Set([
       "_eventsCount"
     ]);
-    
-    Object.getOwnPropertyNames(temp).forEach((key) => {
+
+    Object.getOwnPropertyNames(this).forEach((key) => {
       if (!excludedKeys.has(key)) {
         const val = Reflect.get(this, key);
-        if (typeof val != 'undefined' && ((typeof val == "string" && val != "") || typeof val != "string")) {
-          if ((typeof val) == "object") {
-            if (val.length && val.length > 0) {
-              Reflect.set(json, key, val.toString('hex'));
+
+        if (val !== undefined && val !== '') {
+          if (typeof val === "object" && val !== null) {
+            if (Buffer.isBuffer(val)) {
+              if (val.length > 0) {
+                json[key] = val.toString('hex');
+              }
+            }
+            else if(Array.isArray(val)) {
+              if (val.length > 0) {
+                json[key] = val.toString();
+              }
+            }
+            else {
+              json[key] = val;
             }
           } else {
-            Reflect.set(json, key, val);
+            json[key] = val;
           }
         }
       }
     });
 
-    if (asObject) {
-      return json;
-    } else {
-      return JSON.stringify(json);
-    }
+    return asObject ? json : JSON.stringify(json);
   }
 }
